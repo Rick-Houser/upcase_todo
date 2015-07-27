84fe9a7be687d02c9a595b1b1e41ed8ea88ac7af
FrackenToaster <frakentoaster@gmail.com>
2015-07-27 14:53:57 -0700

    Created user model, refactored todos and application controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,14 @@ class ApplicationController < ActionController::Base
   end
 
   def signed_in?
-  	current_email.present?
-  end
-
-  def current_email
-  	session[:current_email]
+  	current_user.signed_in?
   end
 
   def sign_in_as(email)
   	session[:current_email] = email
+  end
+
+  def current_user
+		User.new(session[:current_email])  	
   end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
 	before_filter :authenticate
 
 	def index
-		@todos = Todo.where(email: current_email)
+		@todos = current_user.todos
 	end
 
 	def new
@@ -10,7 +10,7 @@ class TodosController < ApplicationController
 	end
 
 	def create
-		Todo.create(todo_params.merge(email: session[:current_email]))
+		current_user.todos.create(todo_params)
 		redirect_to todos_path
 	end
 
